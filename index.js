67044170223a1aa3300bf7f024b3355b9152caa0
unknown <nishanthdaredevil1@gmail.com>
2023-03-06 12:57:05 +0530

    1st trial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.render('index.ejs')
+})
+
+app.post('/form-submit', (req, res) => {
+	axios
+		.post('https://hooks.slack.com/services/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', {
+			blocks: [
+				{
+					type: 'section',
+					text: {
+						type: 'mrkdwn',
+						text: `Name: *${req.body.name}*\n\n Email: *${req.body.email}*`,
+					},
+				},
+			],
+		})
+		.then(() => {
+			res.send('Form submitted!')
+		})
+		.catch(() => {
+			res.send('Form submission failed!')
+		})
+})
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000!')
+})
